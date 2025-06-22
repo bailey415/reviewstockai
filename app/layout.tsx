9f1ef4eb5b34e466d84c0aa99417123ad35cec00
Brandon Bailey <bailey@Brandons-MacBook-Pro.local>
2025-06-22 16:53:31 -0700

    feat: add new SVG favicon and update layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -67,6 +67,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       className={`${space_grotesk.variable} scroll-smooth`}
       suppressHydrationWarning
     >
+      <link rel="icon" href={`${basePath}/static/favicons/favicon.svg`} type="image/svg+xml" />
       <link
         rel="apple-touch-icon"
         sizes="76x76"
